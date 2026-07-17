b4206c3bde3e5ef6e032c62743eaad846b847a8b
wwwwxk <1191186801@qq.com>
2026-07-17 23:51:01 +0800

    取消赞助二维码

--- src/config/sponsorConfig.ts
+++ src/config/sponsorConfig.ts
@@ -9,7 +9,7 @@ export const sponsorConfig: SponsorConfig = {
 
 	// 赞助用途说明
 	usage:
-		"赞助将用于服务器、域名续费",
+		"赞助将用于服务器、域名续费，怕有人胡来，现已经取消页面赞助二维码。",
 
 	// 是否显示赞助者列表
 	showSponsorsList: true,
@@ -31,14 +31,14 @@ export const sponsorConfig: SponsorConfig = {
 		//	description: "使用 支付宝 扫码赞助",
 		//	enabled: true,
 		//},
-		{
-			name: "微信",
-			icon: "fa7-brands:weixin",
-			qrCode: "/assets/images/sponsor/wechat.png",
-			link: "",
-			description: "使用 微信 扫码赞助",
-			enabled: true,
-		},
+		//{
+		//	name: "微信",
+		//	icon: "fa7-brands:weixin",
+		//	qrCode: "/assets/images/sponsor/wechat.png",
+		//	link: "",
+		//	description: "使用 微信 扫码赞助",
+		//	enabled: true,
+		//},
 		
 		
 
